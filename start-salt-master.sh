8e4412a36fff12314c7a141b7322ab312bcffa24
Mike Wilson <geekinutah@gmail.com>
2017-08-18 21:57:18 -0600

    Changed logging level, removed uneeded stuff.

--- start-salt-master.sh
+++ start-salt-master.sh
@@ -3,13 +3,10 @@ svn export --force https://github.com/salt-formulas/salt-formulas/trunk/deploy/s
 cd /srv/salt/scripts
 MASTER_HOSTNAME=`hostname -f` /srv/salt/scripts/salt-master-init.sh
 SSH_AUTH_SOCK='/root/ssh-agent.sock' # TODO: This is kinda scary
-. /srv/salt/scripts/salt-master-init.sh
+source /srv/salt/scripts/salt-master-init.sh
 verify-salt-master
 verify-salt-minions
-/usr/bin/salt-master &
-salt-call salt.master
-salt-call salt.client
 salt-call state.apply salt,reclass,ntp --state-output=changes -lerror
 salt-call state.apply --state-output=changes -lerror
 /usr/bin/killall salt-master
-exec /usr/bin/salt-master --log-level=all
+exec /usr/bin/salt-master --log-level=error
