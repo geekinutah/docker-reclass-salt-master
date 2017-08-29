500cebe6cf85ca5e6be799eb32ba9cf3d47e0700
Mike Wilson <geekinutah@gmail.com>
2017-08-29 15:17:13 -0600

    Make container restartable

--- start-salt-master.sh
+++ start-salt-master.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -x
-svn export --force https://github.com/salt-formulas/salt-formulas/trunk/deploy/scripts /srv/salt/scripts
-cd /srv/salt/scripts
-MASTER_HOSTNAME=`hostname -f` /srv/salt/scripts/salt-master-init.sh
-SSH_AUTH_SOCK='/root/ssh-agent.sock' # TODO: This is kinda scary
-source /srv/salt/scripts/salt-master-init.sh
-verify-salt-master
-verify-salt-minions
-salt-call state.apply salt,reclass,ntp --state-output=changes -lerror
-salt-call state.apply --state-output=changes -lerror
-/usr/bin/killall salt-master
-exec /usr/bin/salt-master --log-level=error
+if `grep -q alldone /root/everythingisread`; then
+  exec /usr/bin/salt-master --log-level=error
+else
+  svn export --force https://github.com/salt-formulas/salt-formulas/trunk/deploy/scripts /srv/salt/scripts
+  cd /srv/salt/scripts
+  MASTER_HOSTNAME=`hostname -f` /srv/salt/scripts/salt-master-init.sh
+  SSH_AUTH_SOCK='/root/ssh-agent.sock' # TODO: This is kinda scary
+  source /srv/salt/scripts/salt-master-init.sh
+  verify-salt-master
+  verify-salt-minions
+  salt-call state.apply salt,reclass,ntp --state-output=changes -lerror
+  salt-call state.apply --state-output=changes -lerror
+  /usr/bin/killall salt-master
+  echo 'alldone' > /root/everythingisready
+  exec /usr/bin/salt-master --log-level=error
+fi
